35fd181c2d88bdf104cd1371da196f8464fe0f9c
baban-afnan <shafiumuhd114@gmail.com>
2025-07-28 10:52:14 +0100

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-          '/palmpay/webhook'
+          
     })
+    ->$middleware->validateCsrfTokens(except: [
+            '/palmpay/webhook',
+            
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
