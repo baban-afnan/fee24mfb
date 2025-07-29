a94f7aaf678b41189eaa6d81c06425c351c9a9fb
baban-afnan <shafiumuhd114@gmail.com>
2025-07-29 13:58:03 +0100

    Add referenceId to transactions

--- database/migrations/2025_07_13_145607_create_transactions_table.php
+++ database/migrations/2025_07_13_145607_create_transactions_table.php
@@ -7,20 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
-            
-            
-            $table->string('transaction_ref', 30)->unique();
-            
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->decimal('amount', 12, 2);
+         Schema::create('transactions', function (Blueprint $table) {
+            $table->id(); // id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY
+            $table->string('transaction_ref', 100);
+            $table->string('payer_name')->nullable();
+            $table->string('referenceId')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->string('performed_by')->nullable();
+            $table->decimal('amount', 10, 2);
             $table->decimal('fee', 10, 2)->default(0.00);
-            $table->decimal('net_amount', 12, 2)->default(0.00);
+            $table->decimal('net_amount', 10, 2)->default(0.00);
             $table->string('description')->nullable();
             $table->enum('type', ['credit', 'debit']);
             $table->enum('status', ['pending', 'completed', 'failed', 'reversed'])->default('pending');
-            $table->json('metadata')->nullable();
+            $table->json('metadata')->nullable(); // JSON with validation
             $table->timestamps();
             
             // Indexes for better performance
